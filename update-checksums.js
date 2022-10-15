6ff5438a944901078452b35af7809b241eca7317
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-10-15 19:29:59 +0200

    prevent changing when not needed

--- update-checksums.js
+++ update-checksums.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 const data = require('./data/map.json');
 
+let changed = false
 for (const world in data) {
     for (const island in data[world]) {
         for (const height in data[world][island].images) {
@@ -9,13 +10,17 @@ for (const world in data) {
                 if (typeof data[world][island].images[height][quality] !== 'object') continue;
                 const url = data[world][island].images[height][quality].url.split('/');
                 const file = `./data/${url[url.length - 2]}/${url[url.length - 1]}`;
-                data[world][island].images[height][quality].sha256 = getChecksum(file);
+                const checksum = getChecksum(file);
+                if (checksum !== data[world][island].images[height][quality].sha256) {
+                    data[world][island].images[height][quality].sha256 = checksum
+                    changed = true
+                }
             }
         }
     }
 }
 
-fs.writeFileSync('./data/map.json', JSON.stringify(data, null, 2));
+if (changed) fs.writeFileSync('./data/map.json', JSON.stringify(data, null, 2));
 
 function getChecksum(file) {
     const hash = crypto.createHash('sha256');
